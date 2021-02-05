cc0728e4c40aab64bb11cd712f20dc7e557464fb
Altair Sossai <altairsossai@gmail.com>
2021-02-05 17:10:29 -0300

    Inicio do projeto

---
+++ Zeats.AutoMapper.UnitTest/Extensions/MappingExpressionExtensionsTest.cs
@@ -0,0 +1,53 @@
+using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zeats.AutoMapper.Extensions;
+
+namespace Zeats.AutoMapper.UnitTest.Extensions
+{
+    [TestClass]
+    public class MappingExpressionExtensionsTest
+    {
+        private Mapper _mapper;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var configuration = new MapperConfiguration(c => { c.AddProfile<CustomProfile>(); });
+
+            _mapper = new Mapper(configuration);
+        }
+
+        [TestMethod]
+        public void ForMember()
+        {
+            var source = new Source {Name = "Lorem Ipsum"};
+            var target = _mapper.Map<Target>(source);
+
+            Assert.IsNotNull(target);
+
+            Assert.AreEqual(source.Name, target.CompanyName);
+            Assert.AreEqual("fixed-value", target.Document);
+        }
+
+        private class Source
+        {
+            public string Name { get; set; }
+        }
+
+        public class Target
+        {
+            public string CompanyName { get; set; }
+            public string Document { get; set; }
+        }
+
+        private class CustomProfile : Profile
+        {
+            public CustomProfile()
+            {
+                CreateMap<Source, Target>()
+                    .ForMember(t => t.CompanyName, s => s.Name)
+                    .ForMember(t => t.Document, "fixed-value");
+            }
+        }
+    }
+}
---
+++ Zeats.AutoMapper/Extensions/MappingExpressionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+using AutoMapper;
+
+namespace Zeats.AutoMapper.Extensions
+{
+    public static class MappingExpressionExtensions
+    {
+        public static IMappingExpression<TSource, TDestination> ForMember<TSource, TDestination, TMember, TSourceMember>(
+            this IMappingExpression<TSource, TDestination> mappingExpression,
+            Expression<Func<TDestination, TMember>> destinationMember,
+            Expression<Func<TSource, TSourceMember>> mapExpression)
+        {
+            return mappingExpression
+                .ForMember(destinationMember, options => options.MapFrom(mapExpression));
+        }
+
+        public static IMappingExpression<TSource, TDestination> ForMember<TSource, TDestination, TMember, TValue>(
+            this IMappingExpression<TSource, TDestination> mappingExpression,
+            Expression<Func<TDestination, TMember>> destinationMember,
+            TValue value)
+        {
+            return mappingExpression
+                .ForMember(destinationMember, options => options.MapFrom(m => value));
+        }
+    }
+}
