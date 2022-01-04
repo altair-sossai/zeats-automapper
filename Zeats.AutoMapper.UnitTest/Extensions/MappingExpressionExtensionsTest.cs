f4ddacc6365ebb92a9122c9a5199b66843450843
Altair Sossai <altair_sossai@outlook.com>
2022-01-04 09:55:27 -0800

    namespace fix

--- Zeats.AutoMapper.UnitTest/Extensions/MappingExpressionExtensionsTest.cs
+++ Zeats.AutoMapper.UnitTest/Extensions/MappingExpressionExtensionsTest.cs
@@ -2,51 +2,52 @@ using AutoMapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Zeats.AutoMapper.Extensions;
 
-namespace Zeats.AutoMapper.UnitTest.Extensions;
-
-[TestClass]
-public class MappingExpressionExtensionsTest
+namespace Zeats.AutoMapper.UnitTest.Extensions
 {
-    private Mapper _mapper;
-
-    [TestInitialize]
-    public void Initialize()
+    [TestClass]
+    public class MappingExpressionExtensionsTest
     {
-        var configuration = new MapperConfiguration(c => { c.AddProfile<CustomProfile>(); });
+        private Mapper _mapper;
 
-        _mapper = new Mapper(configuration);
-    }
-
-    [TestMethod]
-    public void ForMember()
-    {
-        var source = new Source {Name = "Lorem Ipsum"};
-        var target = _mapper.Map<Target>(source);
-
-        Assert.IsNotNull(target);
-
-        Assert.AreEqual(source.Name, target.CompanyName);
-        Assert.AreEqual("fixed-value", target.Document);
-    }
-
-    private class Source
-    {
-        public string Name { get; set; }
-    }
-
-    public class Target
-    {
-        public string CompanyName { get; set; }
-        public string Document { get; set; }
-    }
-
-    private class CustomProfile : Profile
-    {
-        public CustomProfile()
+        [TestInitialize]
+        public void Initialize()
         {
-            CreateMap<Source, Target>()
-                .ForMember(t => t.CompanyName, s => s.Name)
-                .ForMember(t => t.Document, "fixed-value");
+            var configuration = new MapperConfiguration(c => { c.AddProfile<CustomProfile>(); });
+
+            _mapper = new Mapper(configuration);
+        }
+
+        [TestMethod]
+        public void ForMember()
+        {
+            var source = new Source {Name = "Lorem Ipsum"};
+            var target = _mapper.Map<Target>(source);
+
+            Assert.IsNotNull(target);
+
+            Assert.AreEqual(source.Name, target.CompanyName);
+            Assert.AreEqual("fixed-value", target.Document);
+        }
+
+        private class Source
+        {
+            public string Name { get; set; }
+        }
+
+        public class Target
+        {
+            public string CompanyName { get; set; }
+            public string Document { get; set; }
+        }
+
+        private class CustomProfile : Profile
+        {
+            public CustomProfile()
+            {
+                CreateMap<Source, Target>()
+                    .ForMember(t => t.CompanyName, s => s.Name)
+                    .ForMember(t => t.Document, "fixed-value");
+            }
         }
     }
 }
